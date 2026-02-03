92dafc62b6d5e7b05636422e5354b5d3b1e8972f
Siraken-05xl <siro04xl@gmail.com>
2026-02-03 13:15:52 +0100

    Timer (Acabado)

---
+++ OUP_Project/Assets/_OUP_Root/Scripts/Timer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    float elapsedTime;
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsedTime / 60f);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60f);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
